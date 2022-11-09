06b5b3b92bc5c7c52d69846bf9de09c7bbd9fb15
Uliame <91170135+Uliame@users.noreply.github.com>
2022-11-09 16:32:38 -0300

    Add files via upload

---
+++ p_sistema_etec/p_sistema_etec/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace p_sistema_etec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_v1 = new System.Windows.Forms.TextBox();
+            this.txt_v2 = new System.Windows.Forms.TextBox();
+            this.bt_calc = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_mf = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema ETEC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Primeira Nota";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Segunda Nota";
+            // 
+            // txt_v1
+            // 
+            this.txt_v1.Location = new System.Drawing.Point(143, 69);
+            this.txt_v1.Name = "txt_v1";
+            this.txt_v1.Size = new System.Drawing.Size(76, 20);
+            this.txt_v1.TabIndex = 3;
+            // 
+            // txt_v2
+            // 
+            this.txt_v2.Location = new System.Drawing.Point(143, 105);
+            this.txt_v2.Name = "txt_v2";
+            this.txt_v2.Size = new System.Drawing.Size(76, 20);
+            this.txt_v2.TabIndex = 4;
+            // 
+            // bt_calc
+            // 
+            this.bt_calc.Location = new System.Drawing.Point(225, 68);
+            this.bt_calc.Name = "bt_calc";
+            this.bt_calc.Size = new System.Drawing.Size(94, 56);
+            this.bt_calc.TabIndex = 5;
+            this.bt_calc.Text = "MÉDIA";
+            this.bt_calc.UseVisualStyleBackColor = true;
+            this.bt_calc.Click += new System.EventHandler(this.bt_calc_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(80, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "MÉDIA FINAL";
+            // 
+            // lbl_mf
+            // 
+            this.lbl_mf.AutoSize = true;
+            this.lbl_mf.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_mf.Location = new System.Drawing.Point(141, 168);
+            this.lbl_mf.Name = "lbl_mf";
+            this.lbl_mf.Size = new System.Drawing.Size(37, 39);
+            this.lbl_mf.TabIndex = 7;
+            this.lbl_mf.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 310);
+            this.Controls.Add(this.lbl_mf);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.bt_calc);
+            this.Controls.Add(this.txt_v2);
+            this.Controls.Add(this.txt_v1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_v1;
+        private System.Windows.Forms.TextBox txt_v2;
+        private System.Windows.Forms.Button bt_calc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_mf;
+    }
+}
+
---
+++ p_sistema_etec/p_sistema_etec/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p_sistema_etec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_calc_Click(object sender, EventArgs e)
+        {
+            double n1, n2, mf;
+
+            n1=double.Parse(txt_v1.Text);
+            n2 = double.Parse(txt_v2.Text);
+
+            mf = (n1 + n2) / 2;
+
+            lbl_mf.Text = mf.ToString();
+        }
+    }
+}
